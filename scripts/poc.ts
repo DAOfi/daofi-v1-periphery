7191bfe70fbbade8d70da2880aa54c10a594629a
you <you@example.com>
2020-11-10 21:15:37 -0800

    Add 'reserve balance' example to poc.ts

--- scripts/poc.ts
+++ scripts/poc.ts
@@ -3,11 +3,11 @@ let poolBalance = 0
 let slope = 1
 let n = 1
 
-function log() {
-  console.log(`tokens minted: ${tokensMinted}, quote reserve: ${poolBalance}`)
+function logCont() {
+  console.log(`tokens minted: ${tokensMinted}, pool balance: ${poolBalance}`)
 }
 
-function init(
+function initCont(
   slopeN: number,
   slopeD: number,
   exp: number
@@ -18,44 +18,111 @@ function init(
   n = exp
 }
 
-function swapToBase(quoteIn: number) {
-  let tokensOut = (((poolBalance + quoteIn) / (slope / (n + 1))) ** (1 / (n + 1))) - tokensMinted
+function buyTokens(poolIn: number) {
+  let tokensOut = (((poolBalance + poolIn) / (slope / (n + 1))) ** (1 / (n + 1))) - tokensMinted
   tokensMinted += tokensOut
-  poolBalance += quoteIn
-  console.log(`swapped ${quoteIn} quote for ${tokensOut} base`)
-  log()
+  poolBalance += poolIn
+  console.log(`swapped ${poolIn} pool in for ${tokensOut} tokens`)
+  logCont()
   return tokensOut
 }
 
-function swapToQuote(tokensIn: number) {
-  let quoteOut = poolBalance - ((slope / (n + 1)) * ((tokensMinted - tokensIn) ** (n + 1)))
+function sellTokens(tokensIn: number) {
+  let poolOut = poolBalance - ((slope / (n + 1)) * ((tokensMinted - tokensIn) ** (n + 1)))
   tokensMinted -= tokensIn
-  poolBalance -= quoteOut
-  console.log(`swapped ${tokensIn} base for ${quoteOut} quote`)
-  log()
+  poolBalance -= poolOut
+  console.log(`swapped ${tokensIn} tokens for ${poolOut} pool out`)
+  logCont()
+  return poolOut
+}
+
+console.log('cont slope 1 exp 1')
+initCont(1, 1, 1)
+sellTokens(buyTokens(100))
+
+console.log('cont slope 1 exp 2')
+initCont(1, 1, 2)
+sellTokens(buyTokens(100))
+
+console.log('cont slope 1/2 exp 1')
+initCont(1, 2, 1)
+sellTokens(buyTokens(100))
+
+console.log('cont slope 1/2 exp 2')
+initCont(1, 2, 2)
+sellTokens(buyTokens(100))
+
+console.log('cont slope 2 exp 1')
+initCont(2, 1, 1)
+sellTokens(buyTokens(100))
+
+console.log('cont slope 2 exp 2')
+initCont(2, 1, 2)
+sellTokens(buyTokens(100))
+
+// lets do similar calculations from a reserve pool
+let baseReserve = 0
+let quoteReserve = 0
+let s = 0
+
+function logRes() {
+  console.log(`base reserve: ${baseReserve}, quote reserve: ${quoteReserve}. s: ${s}`)
+}
+
+function initRes(
+  slopeN: number,
+  slopeD: number,
+  exp: number,
+  baseR: number,
+  quoteR: number,
+) {
+  baseReserve = baseR
+  quoteReserve = quoteR
+  slope = slopeN / slopeD
+  n = exp
+  s = (quoteReserve / (slope / (n + 1))) ** (1 / (n + 1))
+}
+
+function swapToBase(quoteIn: number) {
+  let baseOut = (((quoteReserve + quoteIn) / (slope / (n + 1))) ** (1 / (n + 1))) - s
+  s += baseOut
+  quoteReserve += quoteIn
+  baseReserve -= baseOut
+  console.log(`swapped ${quoteIn} quote for ${baseOut} base`)
+  logRes()
+  return baseOut
+}
+
+function swapToQuote(baseIn: number) {
+  let quoteOut = quoteReserve - ((slope / (n + 1)) * ((s - baseIn) ** (n + 1)))
+  s -= baseIn
+  baseReserve += baseIn
+  quoteReserve -= quoteOut
+  console.log(`swapped ${baseIn} base for ${quoteOut} quote`)
+  logRes()
   return quoteOut
 }
 
-console.log('slope 1 exp 1')
-init(1, 1, 1)
+console.log('res slope 1 exp 1')
+initRes(1, 1, 1, 1000, 1000)
 swapToQuote(swapToBase(100))
 
-console.log('slope 1 exp 2')
-init(1, 1, 2)
+console.log('res slope 1 exp 2')
+initRes(1, 1, 2, 1000, 1000)
 swapToQuote(swapToBase(100))
 
-console.log('slope 1/2 exp 1')
-init(1, 2, 1)
+console.log('res slope 1/2 exp 1')
+initRes(1, 2, 1, 1000, 1000)
 swapToQuote(swapToBase(100))
 
-console.log('slope 1/2 exp 2')
-init(1, 2, 2)
+console.log('res slope 1/2 exp 2')
+initRes(1, 2, 2, 1000, 1000)
 swapToQuote(swapToBase(100))
 
-console.log('slope 2 exp 1')
-init(2, 1, 1)
+console.log('res slope 2 exp 1')
+initRes(2, 1, 1, 1000, 1000)
 swapToQuote(swapToBase(100))
 
-console.log('slope 2 exp 2')
-init(2, 1, 2)
+console.log('res slope 2 exp 2')
+initRes(2, 1, 2, 1000, 1000)
 swapToQuote(swapToBase(100))
