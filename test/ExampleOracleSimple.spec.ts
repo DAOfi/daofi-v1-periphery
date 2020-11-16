0b64f488fb7748f053f8931e93117061724b7a38
you <you@example.com>
2020-11-16 09:33:45 -0800

    Begin refactoring tests

--- test/ExampleOracleSimple.spec.ts
+++ test/ExampleOracleSimple.spec.ts
@@ -38,33 +38,34 @@ describe('ExampleOracleSimple', () => {
   }
 
   beforeEach(async function() {
-    const fixture = await loadFixture(v2Fixture)
+    // const fixture = await loadFixture(v2Fixture)
 
-    token0 = fixture.token0
-    token1 = fixture.token1
-    pair = fixture.pair
-    await addLiquidity()
-    exampleOracleSimple = await deployContract(
-      wallet,
-      ExampleOracleSimple,
-      [fixture.factoryV2.address, token0.address, token1.address],
-      overrides
-    )
+    // token0 = fixture.token0
+    // token1 = fixture.token1
+    // pair = fixture.pair
+    // await addLiquidity()
+    // exampleOracleSimple = await deployContract(
+    //   wallet,
+    //   ExampleOracleSimple,
+    //   [fixture.factoryV2.address, token0.address, token1.address],
+    //   overrides
+    // )
   })
 
   it('update', async () => {
-    const blockTimestamp = (await pair.getReserves())[2]
-    await mineBlock(provider, blockTimestamp + 60 * 60 * 23)
-    await expect(exampleOracleSimple.update(overrides)).to.be.reverted
-    await mineBlock(provider, blockTimestamp + 60 * 60 * 24)
-    await exampleOracleSimple.update(overrides)
+    console.log('TODO')
+    // const blockTimestamp = (await pair.getReserves())[2]
+    // await mineBlock(provider, blockTimestamp + 60 * 60 * 23)
+    // await expect(exampleOracleSimple.update(overrides)).to.be.reverted
+    // await mineBlock(provider, blockTimestamp + 60 * 60 * 24)
+    // await exampleOracleSimple.update(overrides)
 
-    const expectedPrice = encodePrice(token0Amount, token1Amount)
+    // const expectedPrice = encodePrice(token0Amount, token1Amount)
 
-    expect(await exampleOracleSimple.price0Average()).to.eq(expectedPrice[0])
-    expect(await exampleOracleSimple.price1Average()).to.eq(expectedPrice[1])
+    // expect(await exampleOracleSimple.price0Average()).to.eq(expectedPrice[0])
+    // expect(await exampleOracleSimple.price1Average()).to.eq(expectedPrice[1])
 
-    expect(await exampleOracleSimple.consult(token0.address, token0Amount)).to.eq(token1Amount)
-    expect(await exampleOracleSimple.consult(token1.address, token1Amount)).to.eq(token0Amount)
+    // expect(await exampleOracleSimple.consult(token0.address, token0Amount)).to.eq(token1Amount)
+    // expect(await exampleOracleSimple.consult(token1.address, token1Amount)).to.eq(token0Amount)
   })
 })
